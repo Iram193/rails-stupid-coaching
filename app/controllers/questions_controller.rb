e1314cc3e57875463270f2bad24307cfe64b585b
Iram Shehzadi <Shehzadi193@gmail.com>
2020-02-17 15:08:00 +0000

    complete

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @question = params[:question]
+    if @question
+      @result = 'I am going to work'
+      'Great!'
+    elsif @result.include? '?'
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
